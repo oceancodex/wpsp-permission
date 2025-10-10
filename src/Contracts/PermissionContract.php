8687c1ab0cf2b0cfaf37e8721b72790a9ea3b5d6
Khánh Duy Nguyễn <khanhpkvn@gmail.com>
2025-10-10 18:40:38 +0700

    KDN-PC-01: Update

--- src/Contracts/PermissionContract.php
+++ src/Contracts/PermissionContract.php
@@ -3,6 +3,9 @@
 namespace WPSPCORE\Permission\Contracts;
 
 interface PermissionContract {
+
 	public function getName(): string;
+
 	public function getGuardName(): ?string;
+
 }
--- src/Contracts/RoleContract.php
+++ src/Contracts/RoleContract.php
@@ -3,6 +3,9 @@
 namespace WPSPCORE\Permission\Contracts;
 
 interface RoleContract {
+
 	public function getName(): string;
+
 	public function getGuardName(): ?string;
+
 }
